fa469def9cfd4c6615eff7ad48e3ae3e029d857d
kryant <pjserol@free.fr>
2012-12-18 02:20:58 +0100

    Ajout initial des sources

---
+++ Vue/listeBillets.php
@@ -0,0 +1,21 @@
+<?php $titre = 'Mon Blog' ?>
+
+<?php ob_start() ?>
+<?php foreach ($billets as $billet): ?>
+    <article>
+        <header>
+            <h1 class="titreBillet">
+                <a href="<?= $lienBillet . $billet['BIL_ID'] ?>">
+                    <?= $billet['BIL_TITRE'] ?>
+                </a>
+            </h1>
+            <time><?= $billet['BIL_DATE'] ?></time>
+        </header>
+        <p><?= $billet['BIL_CONTENU'] ?></p>
+        <footer class="commentaire"><?= $billet['NB_COM'] ?> commentaire(s)</footer>
+    </article>
+    <hr />
+<?php endforeach; ?>
+<?php $contenu = ob_get_clean() ?>
+
+<?php include 'gabarit.php' ?>
